a95ad257897cdbab3d15bcdcd4d545fc8c7495b7
music.sucks999 <music.sucks999@gmail.com>
2016-02-16 12:46:30 +0300

    Java code conventions

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,17 @@
-/**
- * Created by Денис on 15.02.2016.
- */
+
 public class Main {
     public static void main(String[] args) {
-        Element1 a = new Element1();
-        Element2 b = new Element2();
+        E1 a = new E1();
         V1 someGuy = new V1();
 
         a.accept(someGuy);
-        b.accept(someGuy);
-
     }
 
 
 
 
 }
-//Интерфейсы
-interface Visitor {
-    void visit(Element1 e);
-    void visit(Element2 e);
-}
 
-interface Element {
-    void accept(Visitor v);
-}
-// Элементы
-class Element1 implements Element {
-    public void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-class Element2 implements Element {
-    public void accept(Visitor v) {
-        v.visit(this);
-    }
-}
-//Посетители
-class V1 implements Visitor {
-    public void visit(Element1 e) {
-        System.out.println("посетитель класса V1 посетил объект класса Element1"); //smth
-    }
-    public void visit(Element2 e) {
-        System.out.println("посетитель класса V1 посетил объект класса Element2"); //smth
-    }
 
-}
 
 
